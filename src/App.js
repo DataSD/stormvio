ed4309fe0261051fb3f5e7c88d6446f3b7563c03
Maksim Pecherskiy <max@maksimize.com>
2018-10-16 16:57:10 -0700

    Initial.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,132 @@
 import React, { Component } from 'react';
+import {render} from 'react-dom'
+import MapGL from 'react-map-gl'
+import ControlPanel from './control-panel';
+
 import logo from './logo.svg';
 import './App.css';
+//import 'mapbox-gl/dist/mapbox-gl.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+import {defaultMapStyle, dataLayer} from './map-style.js';
+import {updatePercentiles} from './utils';
+import {fromJS} from 'immutable';
+import {json as requestJson} from 'd3-request';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoicXVhbmRhcnkiLCJhIjoiY2pndmVrcHU5MHJ4cTJxcDgwcjJubnBucyJ9.wRn-2fAYIED8VQDqo0M1CQ';
+
+export default class App extends Component {
+
+  state = {
+    mapStyle: defaultMapStyle,
+    year: 2015,
+    data: null,
+    hoveredFeature: null,
+    viewport: {
+      latitude: 40,
+      longitude: -100,
+      zoom: 3,
+      bearing: 0,
+      pitch: 0,
+      width: 500,
+      height: 500
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resize);
+    this._resize();
+
+    requestJson('data/us-income.geojson', (error, response) => {
+      if (!error) {
+        this._loadData(response);
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize);
+  }
+
+  _resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: this.props.width || window.innerWidth,
+        height: this.props.height || window.innerHeight
+      }
+    });
+  };
+
+  _loadData = data => {
+
+    updatePercentiles(data, f => f.properties.income[this.state.year]);
+
+    const mapStyle = defaultMapStyle
+      // Add geojson source to map
+      .setIn(['sources', 'incomeByState'], fromJS({type: 'geojson', data}))
+      // Add point layer to map
+      .set('layers', defaultMapStyle.get('layers').push(dataLayer));
+
+    this.setState({data, mapStyle});
+  };
+
+  _updateSettings = (name, value) => {
+    if (name === 'year') {
+      this.setState({year: value});
+
+      const {data, mapStyle} = this.state;
+      if (data) {
+        updatePercentiles(data, f => f.properties.income[value]);
+        const newMapStyle = mapStyle.setIn(['sources', 'incomeByState', 'data'], fromJS(data));
+        this.setState({mapStyle: newMapStyle});
+      }
+    }
+  };
+
+  _onViewportChange = viewport => this.setState({viewport});
+
+  _onHover = event => {
+    const {features, srcEvent: {offsetX, offsetY}} = event;
+    const hoveredFeature = features && features.find(f => f.layer.id === 'data');
+
+    this.setState({hoveredFeature, x: offsetX, y: offsetY});
+  };
+
+  _renderTooltip() {
+    const {hoveredFeature, year, x, y} = this.state;
+
+    return hoveredFeature && (
+      <div className="tooltip" style={{left: x, top: y}}>
+        <div>State: {hoveredFeature.properties.name}</div>
+        <div>Median Household Income: {hoveredFeature.properties.value}</div>
+        <div>Percentile: {hoveredFeature.properties.percentile / 8 * 100}</div>
       </div>
     );
   }
+
+  render() {
+
+    const {viewport, mapStyle} = this.state;
+
+    return (
+      <div>
+        <MapGL
+          {...viewport}
+          mapStyle={mapStyle}
+          onViewportChange={this._onViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          onHover={this._onHover} >
+
+          {this._renderTooltip()}
+
+        </MapGL>
+
+        <ControlPanel containerComponent={this.props.containerComponent}
+          settings={this.state} onChange={this._updateSettings} />
+      </div>
+    );
+  }
+
 }
 
-export default App;
+

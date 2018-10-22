714eec1b300bcaf80528482daec3270dced7d5e7
Maksim Pecherskiy <max@maksimize.com>
2018-10-22 11:35:29 -0700

    Displaying dots.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,69 @@
 import React, { Component } from 'react';
-import {render} from 'react-dom'
-import MapGL from 'react-map-gl'
+import MapGL, {Marker, NavigationControl} from 'react-map-gl'
 import ControlPanel from './control-panel';
 
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-//import 'mapbox-gl/dist/mapbox-gl.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 import {defaultMapStyle, dataLayer} from './map-style.js';
 import {updatePercentiles} from './utils';
 import {fromJS} from 'immutable';
 import {json as requestJson} from 'd3-request';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Row,
+  Col,
+  Button } from 'reactstrap';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoicXVhbmRhcnkiLCJhIjoiY2pndmVrcHU5MHJ4cTJxcDgwcjJubnBucyJ9.wRn-2fAYIED8VQDqo0M1CQ';
 
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+};
+
+
 export default class App extends Component {
 
-  state = {
-    mapStyle: defaultMapStyle,
-    year: 2015,
-    data: null,
-    hoveredFeature: null,
-    viewport: {
-      latitude: 40,
-      longitude: -100,
-      zoom: 3,
-      bearing: 0,
-      pitch: 0,
-      width: 500,
-      height: 500
-    }
-  };
+  constructor(props) {
+    super (props);
+    this.toggle = this.toggle.bind(this);
+    console.log(defaultMapStyle);
+
+    this.state = {
+      mapStyle: defaultMapStyle,
+      year: 2015,
+      data: null,
+      hoveredFeature: null,
+      navbarOpen: false,
+      viewport: {
+        latitude: 32.7157,
+        longitude: -117.1611,
+        zoom: 12,
+        bearing: 0,
+        pitch: 0,
+        width: 500,
+        height: 500
+      }
+    };
+
+  }
 
   componentDidMount() {
     window.addEventListener('resize', this._resize);
     this._resize();
 
-    requestJson('data/us-income.geojson', (error, response) => {
+    requestJson('data/tsw_violations_merged.geojson', (error, response) => {
       if (!error) {
         this._loadData(response);
       }
@@ -45,6 +72,10 @@ export default class App extends Component {
 
   componentWillUnmount() {
     window.removeEventListener('resize', this._resize);
+  }
+
+  toggle () {
+    this.setState({ navbarOpen: !this.state.navbarOpen });
   }
 
   _resize = () => {
@@ -58,12 +89,14 @@ export default class App extends Component {
   };
 
   _loadData = data => {
+    console.log(data);
+    console.log(defaultMapStyle.get('layers'));
 
-    updatePercentiles(data, f => f.properties.income[this.state.year]);
+    //updatePercentiles(data, f => f.properties.income[this.state.year]);
 
     const mapStyle = defaultMapStyle
       // Add geojson source to map
-      .setIn(['sources', 'incomeByState'], fromJS({type: 'geojson', data}))
+      .setIn(['sources', 'tsw_violations'], fromJS({type: 'geojson', data}))
       // Add point layer to map
       .set('layers', defaultMapStyle.get('layers').push(dataLayer));
 
@@ -110,6 +143,21 @@ export default class App extends Component {
 
     return (
       <div>
+        <Navbar color="inverse" light expand="md">
+          <NavbarBrand href="/">StormVio</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.navbarOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                  <NavItem>
+                      <NavLink href="/components/">Components</NavLink>
+                  </NavItem>
+                  <NavItem>
+                        <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+                  </NavItem>
+              </Nav>
+            </Collapse>
+        </Navbar>
+
         <MapGL
           {...viewport}
           mapStyle={mapStyle}
@@ -119,10 +167,14 @@ export default class App extends Component {
 
           {this._renderTooltip()}
 
-        </MapGL>
+
+        <div className="nav" style={navStyle}>
+          <NavigationControl onViewportChange={this._updateViewport} />
+        </div>
 
         <ControlPanel containerComponent={this.props.containerComponent}
           settings={this.state} onChange={this._updateSettings} />
+        </MapGL>
       </div>
     );
   }
